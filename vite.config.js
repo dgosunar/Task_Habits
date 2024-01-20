f041e6ff241ff510001848fdca0901e082640a1a
dgosunar <dgosunar@unal.edu.co>
2024-01-20 14:51:28 -0500

    carga de configuracion deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Task_React_Projects/'
+  }
+
+  return config
 })
