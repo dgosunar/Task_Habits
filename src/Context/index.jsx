9dfe39ea171679941739494b083fabfec08fe714
dgosunar <dgosunar@unal.edu.co>
2024-01-20 21:44:57 -0500

    v2

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocalStorage } from "../hooks/useLocalStorage";
+import { useLocalStorage } from "../Hooks/useLocalStorage";
 
 const Context = React.createContext();
 
