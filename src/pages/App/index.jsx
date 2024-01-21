f2c117bebd32eac7a2b29aea156024e27d5be309
dgosunar <dgosunar@unal.edu.co>
2024-01-20 21:37:34 -0500

    v2

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useRoutes, BrowserRouter } from 'react-router-dom'
 
-import Home from '../home'
-import Tasks from '../tasks'
-import Habits from '../habits'
-import Contact from '../contact'
-import NotFound from '../notFound'
+import Home from '../home';
+import Tasks from '../tasks';
+import Habits from '../habits';
+import Contact from '../contact';
+import NotFound from '../notFound';
 
 import '../../Styles/App.css'
 
