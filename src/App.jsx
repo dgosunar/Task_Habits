14003fc3e14b420cf63b3df09e9cc80ad845b408
dgosunar <dgosunar@unal.edu.co>
2024-01-20 22:45:14 -0500

    React.js con Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useRoutes, BrowserRouter } from 'react-router-dom'
 
-import Home from '../Home';
-import Tasks from '../tasks';
-import Habits from '../Habits';
-import Contact from '../Contact';
-import NotFound from '../NotFound';
+import Home from './pages/Home';
+import Tasks from './pages/tasks';
+import Habits from './pages/Habits';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
-import '../../Styles/App.css'
+import './styles/App.css'
 
 const AppRoutes = () => {
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './Styles/index.css'
-import App from './pages/App'
+import App from './App.jsx'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
